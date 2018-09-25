dae95812dd23a70209a0edf79824b1f42310178c
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-09-25 19:21:11 +0200

    basne1.0

--- basne.py
+++ basne.py
@@ -38,7 +38,7 @@ class Poem:
         for _ in range(countW - 1):
             line += self.words[random.randint(0, len(self.words) - 1)]
             line += " "
-        line += self.rhymes[random.randint(0, len(self.words) - 1)].lower()
+        line += self.rhymes[random.randint(0, len(self.rhymes) - 1)].lower()
         line += "."
         line = line[0].upper() + line[1:]
         return line
@@ -49,8 +49,19 @@ class Poem:
         """
         poem = ""
         for _ in range(countL):
-           poem += self.getLine(random.randint(4,6)) + "\n"
+           poem += self.getLine(random.randint(4,5)) + "\n"
         return poem
+
+
+def getAnswer(string, var):
+    while True:
+        ans = input(string)
+        if ans in var:
+            return ans
+        else:
+            print("Odpovedi jsou {}\nNe {}.".format(var, ans))
+            continue
+
 
 if __name__ == "__main__":
     while True:
@@ -62,11 +73,20 @@ if __name__ == "__main__":
             sys.exit(1)
         poem = Poem(sys.argv[1],sys.argv[2])
         print(poem.generatePoem(x).rstrip())
-        check = input("Chcete další báseň? [a/n] ")
+        check = getAnswer("Chcete další báseň [a/n]? ", ["a", "n"])
         if check == "a":
             continue
         elif check == "n":
             break
         else:
-            print("Odpověd je 'a', nebo 'n'.\nNe {}.".format(check))
-            sys.exit(2)
+            raise ValueError("Chybný stav!")
+
+
+def getAnswer(string, var):
+    while True:
+        ans = input(string)
+        if ans in var:
+            return ans
+        else:
+            print("Odpovedi jsou {}\nNe {}.".format(var, ans))
+            continue
