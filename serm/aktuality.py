c17d247bc2e07f033718cac226153180b33f3d8d
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-09-06 12:27:44 +0200

    mail only with changes

--- serm/aktuality.py
+++ serm/aktuality.py
@@ -4,7 +4,24 @@ import smtplib
 import hashlib
 import urllib.request
 
+# pošli na mail
+def mail():
+    with open("pass.txt", "r", encoding="utf-8") as text:
+        udaje = list(text)
+    udaje = [s.rstrip() for s in udaje]
+    jmeno, heslo = tuple(udaje)    # uloží údaje do samostatných proměných
+    komu = "prokop.paruzek@paruzkovi.cz"
+    telo += "Důležité odkazy: http://www.czechfencing.cz/portal/news a http://www.czechfencing.cz/portal/members/detail/2477\nTvůj bot"
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as m:
+        m.ehlo()
+        m.login(jmeno, heslo)
+        subject = "Změna na stránkách svazu"
+        msg = "FROM: " + jmeno + "\n" + "TO: " + komu + "\n" + "SUBJECT: " + subject + "\n\n" + telo
+        msg = msg.encode()
+        m.sendmail(jmeno, komu, msg)
+
 telo = ""
+send_mail = False
 
 # Novinky
 try:
@@ -16,12 +33,14 @@ try:
         oh = news.read()
         if h != oh:
             telo += "Změna v novinkách.\n"
+            send_mail = True
 except FileNotFoundError:
     s = urllib.request.urlopen("http://www.czechfencing.cz/portal/news")
     m = hashlib.md5()
     m.update(s.read())
     h = m.hexdigest()
     telo += "Změna v novinkách.\n"
+    send_mail = True
 with open("news.md5", "w") as news:
     news.write(h)
 
@@ -35,26 +54,16 @@ try:
         oh = status.read()
         if h != oh:
             telo += "Změna ve statutu.\n"
+            send_mail = True
 except FileNotFoundError:
     s = urllib.request.urlopen("http://www.czechfencing.cz/portal/members/detail/2477")
     m = hashlib.md5()
     m.update(s.read())
     h = m.hexdigest()
     telo += "Změna ve statutu\n"
+    send_mail = True
 with open("status.md5", "w") as news:
     news.write(h)
 
-# pošli na mail
-with open("pass.txt", "r", encoding="utf-8") as text:
-    udaje = list(text)
-udaje = [s.rstrip() for s in udaje]
-jmeno, heslo = tuple(udaje)    # uloží údaje do samostatných proměných
-komu = "prokop.paruzek@paruzkovi.cz"
-telo += "Důležité odkazy: http://www.czechfencing.cz/portal/news a http://www.czechfencing.cz/portal/members/detail/2477\nTvůj bot"
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as m:
-    m.ehlo()
-    m.login(jmeno, heslo)
-    subject = "Změna na stránkách svazu"
-    msg = "FROM: " + jmeno + "\n" + "TO: " + komu + "\n" + "SUBJECT: " + subject + "\n\n" + telo
-    msg = msg.encode()
-    m.sendmail(jmeno, komu, msg)
+if send_mail:
+    mail()
